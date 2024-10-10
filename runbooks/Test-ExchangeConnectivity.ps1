74ae1160ce3006e0f4597a879cbc49acbc0ee02a
Tommy Johannessen <tommy.johannessen@crayon.com>
2024-10-10 12:06:43 +0200

    Switched to Microsoft Graph cmdlet for retrieving Organization domain
    Signed-off-by: Tommy Johannessen <tommy.johannessen@crayon.com>

--- runbooks/Test-ExchangeConnectivity.ps1
+++ runbooks/Test-ExchangeConnectivity.ps1
@@ -1,15 +1,21 @@
 $ErrorActionPreference = 'Stop'
 
 try {
-    Write-Output "Connecting to Azure"
-    $AzureConnectionResult = Connect-AzAccount -Identity
-    Write-Output "Retrieving default tenant domain"
-    $DefaultDomain = Get-AzTenant | Select-Object -ExpandProperty DefaultDomain
+
+    Write-Output "Connecting to Microsoft Graph"
+    $MGConnectionResult = Connect-MgGraph -ManagedIdentity -NoWelcome
+
+    Write-Output "Retrieving initial tenant domain"
+    $InitialDomain = Get-MgDomain | Where-Object { $_.IsInitial -eq $True } | Select-Object -ExpandProperty Id
+
     Write-Output "Connecting to Exchange Online"
-    $ExchangeConnectionResult = Connect-ExchangeOnline -ManagedIdentity -Organization $DefaultDomain
+    $ExchangeConnectionResult = Connect-ExchangeOnline -ManagedIdentity -Organization $InitialDomain
+
     Write-Output "Running Exchange cmdlet for testing"
     $AcceptedDomains = Get-AcceptedDomain
-    Write-Output "Everything works!"
+
+    Write-Output "Everything works! You can now create new runbooks or replace this one to do your Exchange management tasks."
+
 } catch {
-    Write-Error "There seems to be an error`: $_"
+    Write-Error $_
 }
